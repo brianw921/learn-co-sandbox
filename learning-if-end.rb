4761b7fea464a9e72fdf81291f122cbcd462329e
Brian Wong <bwong92@gmail.com>
2019-07-29 21:20:19 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,58 @@
+# run_code_inside = false
+# puts "code before if...end"
+# if run_code_inside
+#     puts "code inside"
+#   end
+#   puts "code after if...end"
+
+# chance_of_rain = 0.7
+# puts "Let's go outside!"
+# if chance_of_rain > 0.5
+#   puts "Pack an umbrella!"
+# else
+#   puts "Enjoy the fine day!"
+# end
+# puts "Oh, and always wear sunscreen!"
+
+# chance_of_rain = 1
+# if chance_of_rain <= 0.25
+#   puts "Pack a sun shelter!"
+# elsif (chance_of_rain > 0.25 &&  chance_of_rain < 0.75)
+#   puts "Pack an umbrella!"
+# else
+#   puts "Stay and home and read Hegel."
+# end
+
+# 3.times do 
+#   puts "i ran."
+# end
+
+# puts "You know what year it is??"
+# this_year = 2020
+# # puts "Hey, it's 2019!" if this_year == 2019
+# puts "hey , it's not 2019!" unless this_year == 2019
+
+# puts Time.now.year
+
+# count = 0 
+# n = 3
+# loop do 
+#   break if count >= n
+#   puts "i ran."
+#   count += 1
+# end
+
+# count = 0
+# n = 3
+# while true do
+#   break if count >= n
+#   puts "I ran."
+#   count += 1
+# end
+
+counter = 18
+until counter == 20 
+puts "the current num is less than 20"
+counter += 1 
+end
+
